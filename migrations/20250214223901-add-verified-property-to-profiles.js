d9ff9f8bd71fa638c74f14878f7d7c529ac792b2
iris <iris@heyiris.dev>
2025-02-14 22:59:34 +0000

    feat(db): add migration for adding verified property

---
+++ migrations/20250214223901-add-verified-property-to-profiles.js
@@ -0,0 +1,20 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		const tableInfo = await queryInterface.describeTable("profiles");
+		if (tableInfo && !tableInfo.verified) {
+			await queryInterface.addColumn("profiles", "verified", {
+				type: Sequelize.BOOLEAN,
+				allowNull: false,
+				defaultValue: false,
+			});
+		}
+	},
+
+	async down(queryInterface, Sequelize) {
+		const tableInfo = await queryInterface.describeTable("profiles");
+		if (tableInfo.verified) {
+			await queryInterface.removeColumn("profiles", "verified");
+		}
+	},
+};
